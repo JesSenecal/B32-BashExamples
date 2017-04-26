cbf9a1263592eddddeecb60a415be25fc77eb5cb
Jessica Senecal <senecal.j.94@gmail.com>
2017-04-26 14:55:55 -0400

    / sortes de boucle

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -4,3 +4,18 @@ for ((i=0;i<5;i++))
 do
 	echo $i
 done
+
+resultat=0
+
+while test $resultat ="o"
+do 
+	read -p "Entrez une lettre :" resultat
+done
+
+maListe=`ls`
+
+for element in $maListe
+do
+	echo $element
+done
+
