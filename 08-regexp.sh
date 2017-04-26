0771fb5667dcbe1b92b13fab2392217178e68b3a
Jessica Senecal <senecal.j.94@gmail.com>
2017-04-26 15:10:57 -0400

    Expression

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+#Comme read -p  "Lettre :" lettre
+echo -n "Lettre:"
+read lettre 
+
+# ~= regexp!
+#il faut que ca commence par  0-9 au moins 1 et il faut que ca termine par
+if [[$lettres=~ ^[0-9]+$ ]]
+#if [[ $lettre =~[a-zA-Z\s]+$ ]] lettre entre A-Z avec espace
+then
+	echo "C'est une nombre"
+fi
